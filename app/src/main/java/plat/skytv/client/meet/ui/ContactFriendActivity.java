b918012ba7c6d974341279583a632c5756f7567c
boshuai.li <522633682@qq.com>
2020-07-27 21:40:46 +0800

    获取联系热人 输出

--- app/src/main/java/plat/skytv/client/meet/ui/ContactFriendActivity.java
+++ app/src/main/java/plat/skytv/client/meet/ui/ContactFriendActivity.java
@@ -9,7 +9,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.liboshuai.framework.base.BaseBackActivity;
+import com.liboshuai.framework.bmob.BmobManager;
+import com.liboshuai.framework.bmob.IMUser;
+import com.liboshuai.framework.utils.LogUtils;
+import com.liboshuai.framework.utils.ToastUtil;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import plat.skytv.client.meet.R;
 
 /**
@@ -20,6 +30,7 @@ import plat.skytv.client.meet.R;
 public class ContactFriendActivity extends BaseBackActivity {
 
     private RecyclerView mContactView;
+    private Map<String, String> mContactsMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +43,50 @@ public class ContactFriendActivity extends BaseBackActivity {
         mContactView = (RecyclerView) findViewById(R.id.mContactView);
         mContactView.setLayoutManager(new LinearLayoutManager(this));
         mContactView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        
+
         loadContacts();
+
+        loadUsers();
+    }
+
+    /**
+     * 加载用户
+     */
+    private void loadUsers() {
+        if (mContactsMap.size() > 0) {
+            for (Map.Entry<String, String> entry : mContactsMap.entrySet()) {
+                BmobManager.getInstance().queryPhoneUser(entry.getValue(), new FindListener<IMUser>() {
+                    @Override
+                    public void done(List<IMUser> list, BmobException e) {
+                        if (e == null) {
+                            if (list != null && list.size() > 0) {
+                                IMUser imUser = list.get(0);
+                            }
+                        } else {
+                            LogUtils.i("loadUsers queryPhoneUser error = " + e.toString());
+                            ToastUtil.showTextToast(ContactFriendActivity.this, "Error = " + e.toString());
+                        }
+                    }
+                });
+            }
+        }
+
     }
 
     private void loadContacts() {
-        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null);
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null);
 
         String name;
         String phone;
         while (cursor.moveToNext()) {
-            name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            phone = phone.replace(" ", "").replace("-", "");
+
+            LogUtils.i("loadContacts name = " + name + ", phone = " + phone);
+
+            mContactsMap.put(name, phone);
         }
     }
 }
