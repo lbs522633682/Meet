e00c19d68314642e0a1dee583a7b6a565edad43a
boshuai.li <522633682@qq.com>
2020-07-29 10:24:44 +0800

    万能的 CommonViewholder 提供外部访问view的方法

--- framework/src/main/java/com/liboshuai/framework/adapter/CommonViewHolder.java
+++ framework/src/main/java/com/liboshuai/framework/adapter/CommonViewHolder.java
@@ -1,5 +1,6 @@
 package com.liboshuai.framework.adapter;
 
+import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,10 +12,42 @@ import androidx.recyclerview.widget.RecyclerView;
  * Description:万能的viewHolder
  */
 public class CommonViewHolder extends RecyclerView.ViewHolder {
-    public CommonViewHolder(View itemView) {
-        super(itemView);
+
+    private SparseArray<View> mViews;
+    private View mContentView;
+
+    /**
+     * 获取 CommonViewHolder 实体
+     *
+     * @param parent
+     * @param layoutId
+     * @return
+     */
+    public static CommonViewHolder getViewHolder(ViewGroup parent, int layoutId) {
+        return new CommonViewHolder(View.inflate(parent.getContext(), layoutId, null));
     }
 
-    public static CommonViewHolder getViewHolder(ViewGroup parent, int layoutId) {
+    public CommonViewHolder(View itemView) {
+        super(itemView);
+        mViews = new SparseArray<>();
+        mContentView = itemView;
     }
+
+    /**
+     * 提供 外部的访问view的方法
+     * @param viewId
+     * @param <T>
+     * @return
+     */
+    public <T extends View> T getView(int viewId) {
+        View view = mViews.get(viewId);
+        if (mViews.get(viewId) == null) {
+            view = mContentView.findViewById(viewId);
+            mViews.put(viewId, view);
+        }
+
+        return (T) view;
+    }
+
+
 }
